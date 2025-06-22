85c6c5187384590c7a0658f83d28dbba70e0fac8
Dawit Firew <135819244+DavisonFR@users.noreply.github.com>
2025-06-22 22:11:54 +0300

    Update MovieCard.tsx

---
+++ alx-movie-app/components/commons/MovieCard.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+interface MovieCardProps {
+  title: string;
+  image: string;
+  year: number;
+}
+
+const MovieCard = ({ title, image, year }: MovieCardProps) => {
+  return (
+    <div className="bg-white shadow-md rounded overflow-hidden w-[200px]">
+      <img src={image} alt={title} className="w-full h-[300px] object-cover" />
+      <div className="p-4">
+        <h3 className="text-lg font-semibold">{title}</h3>
+        <p className="text-sm text-gray-500">{year}</p>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
